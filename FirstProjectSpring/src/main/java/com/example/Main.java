fd55caa47da0a61bb153a2ac204070ca9e7145d4
jpcpsgodoy <jpcpsgodoy@gmail.com>
2025-04-12 00:33:14 -0300

    criação do repositório e término da primeira aplicação Spring

---
+++ FirstProjectSpring/src/main/java/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class Main {
+    public static void main(String[] args) {
+        // Load XML-based configuration
+        ApplicationContext xmlContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        HelloWorld helloWorldXml = (HelloWorld) xmlContext.getBean("estugueraguejhonson");
+        helloWorldXml.printMessage(); // Output: Message: Hello, World from XML!
+    }
+}
